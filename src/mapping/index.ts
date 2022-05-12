6f0173a94fa64b70b7b574aa88c37617adb187d5
Linguists <95207870+linguists@users.noreply.github.com>
2022-05-12 23:22:35 +0800

    refactor: make mapping readable

--- src/mapping/index.ts
+++ src/mapping/index.ts
@@ -3,13 +3,15 @@ import {
   BigInt,
   log,
   json,
+  JSONValue,
+  TypedMap,
   BigDecimal,
 } from "@graphprotocol/graph-ts";
 import {
   Price,
   FtTransfer,
   TotalSwapFee,
-  Version
+  Version,
 } from "../../generated/schema";
 import { getOrInitUser } from "./helper/initializer";
 
@@ -22,274 +24,221 @@ export function handleReceipt(receipt: near.ReceiptWithOutcome): void {
 
 function handleAction(
   action: near.ActionValue,
-  receiptWithOutcome: near.ReceiptWithOutcome
+  receipt: near.ReceiptWithOutcome
 ): void {
   if (action.kind != near.ActionKind.FUNCTION_CALL) {
     return;
   }
-  const timestamp = receiptWithOutcome.block.header.timestampNanosec;
-  const blockHeight = receiptWithOutcome.block.header.height;
-  const epochId = receiptWithOutcome.block.header.epochId;
-  const outcome = receiptWithOutcome.outcome;
-  const receiptHash = receiptWithOutcome.receipt.id.toBase58();
-  const functionCall = action.toFunctionCall();
-  const methodName = functionCall.methodName;
+  const outcome = receipt.outcome;
+  const methodName = action.toFunctionCall().methodName;
 
+  for (let logIndex = 0; logIndex < outcome.logs.length; logIndex++) {
+    let outcomeLog = outcome.logs[logIndex].toString();
+    if (outcomeLog.startsWith("EVENT_JSON:")) {
+      outcomeLog = outcomeLog.replace("EVENT_JSON:", "");
+      const jsonData = json.try_fromString(outcomeLog);
+      const jsonObject = jsonData.value.toObject();
+      const event = jsonObject.get("event")!;
+      const dataArr = jsonObject.get("data")!.toArray();
+      const dataObj: TypedMap<string, JSONValue> = dataArr[0].toObject();
+
+      handleEvent(methodName, event.toString(), dataObj, receipt);
+    }
+  }
+}
+
+function handleEvent(
+  method: string,
+  event: string,
+  data: TypedMap<string, JSONValue>,
+  receipt: near.ReceiptWithOutcome
+): void {
   if (
-    methodName == "stake" ||
-    methodName == "deposit_and_stake" ||
-    methodName == "stake_all"
+    (method == "stake" ||
+      method == "deposit_and_stake" ||
+      method == "stake_all") &&
+    event == "stake"
   ) {
-    for (let logIndex = 0; logIndex < outcome.logs.length; logIndex++) {
-      let outcomeLog = outcome.logs[logIndex].toString();
-      if (outcomeLog.startsWith("EVENT_JSON:")) {
-        outcomeLog = outcomeLog.replace("EVENT_JSON:", "");
-        const jsonData = json.try_fromString(outcomeLog);
-        const jsonObject = jsonData.value.toObject();
-        const event = jsonObject.get("event")!;
+    handleStakeEvent(data, receipt);
+  } else if (
+    (method == "unstake" || method == "unstake_all") &&
+    event == "unstake"
+  ) {
+    handleUnstakeEvent(data, receipt);
+  } else if (method == "instant_unstake" && event == "instant_unstake") {
+    handleInstantUnstakeEvent(data);
+  } else if (
+    (method == "ft_transfer" ||
+      method == "ft_transfer_call" ||
+      method == "remove_liquidity") &&
+    event == "ft_transfer"
+  ) {
+    handleFtTransferEvent(data, receipt);
+  } else if (
+    method == "instant_unstake" &&
+    event == "liquidity_pool_swap_fee"
+  ) {
+    handleLiquidityPoolSwapFeeEvent(data, receipt);
+  }
+}
 
-        if (event.toString() == "stake") {
-          // parse event
-          const data = jsonObject.get("data")!;
-          const dataArr = data.toArray();
-          const dataObj = dataArr[0].toObject();
-          const accountId = dataObj.get("account_id")!.toString();
-          const stakeAmountStr = dataObj.get("staked_amount")!.toString();
-          const mintedSharesStr = dataObj
-            .get("minted_stake_shares")!
-            .toString();
-          const stakeAmount = BigInt.fromString(stakeAmountStr);
-          const minted_shares = BigInt.fromString(mintedSharesStr);
+// ----- Event Mapping Handlers ------
 
-          // update user
-          let user = getOrInitUser(accountId);
-          user.stakedNear = user.stakedNear.plus(stakeAmount);
-          user.mintedLinear = user.mintedLinear.plus(minted_shares);
-          if (user.firstStakingTime.isZero()) {
-            user.firstStakingTime = BigInt.fromU64(timestamp);
-          }
-          user.save();
+function handleStakeEvent(
+  data: TypedMap<string, JSONValue>,
+  receipt: near.ReceiptWithOutcome
+): void {
+  const timestamp = receipt.block.header.timestampNanosec;
+  const blockHeight = receipt.block.header.height;
 
-          // update price
-          let price = new Price(blockHeight.toString());
-          let minted_sharesFloat = BigDecimal.fromString(mintedSharesStr);
-          let stakeAmountFloat = BigDecimal.fromString(stakeAmountStr);
-          price.linearAmount = minted_sharesFloat;
-          price.nearAmount = stakeAmountFloat;
-          price.timestamp = timestamp.toString();
-          price.price = stakeAmountFloat.div(minted_sharesFloat);
-          price.save();
-        }
-      }
-    }
-  } else if (methodName == "unstake") {
-    for (let logIndex = 0; logIndex < outcome.logs.length; logIndex++) {
-      let outcomeLog = outcome.logs[logIndex].toString();
-      if (outcomeLog.startsWith("EVENT_JSON:")) {
-        outcomeLog = outcomeLog.replace("EVENT_JSON:", "");
-        const jsonData = json.try_fromString(outcomeLog);
-        const jsonObject = jsonData.value.toObject();
-        const event = jsonObject.get("event")!;
+  // parse event
+  const accountId = data.get("account_id")!.toString();
+  const stakeAmountStr = data.get("staked_amount")!.toString();
+  const mintedSharesStr = data.get("minted_stake_shares")!.toString();
+  const stakeAmount = BigInt.fromString(stakeAmountStr);
+  const mintedShares = BigInt.fromString(mintedSharesStr);
+  const mintedSharesFloat = BigDecimal.fromString(mintedSharesStr);
+  const stakeAmountFloat = BigDecimal.fromString(stakeAmountStr);
 
-        if (event.toString() == "unstake") {
-          // parse event
-          const data = jsonObject.get("data")!;
-          const dataArr = data.toArray();
-          const dataObj = dataArr[0].toObject();
-          const accountId = dataObj.get("account_id")!.toString();
-          const unstakeAmountStr = dataObj.get("unstaked_amount")!.toString();
-          const burnedSharesStr = dataObj.get("burnt_stake_shares")!.toString();
-          const unstakeAmount = BigInt.fromString(unstakeAmountStr);
-          const burnedShares = BigInt.fromString(burnedSharesStr);
+  // update user
+  let user = getOrInitUser(accountId);
+  user.stakedNear = user.stakedNear.plus(stakeAmount);
+  user.mintedLinear = user.mintedLinear.plus(mintedShares);
+  if (user.firstStakingTime.isZero()) {
+    user.firstStakingTime = BigInt.fromU64(timestamp);
+  }
+  user.save();
 
-          // update user
-          let user = getOrInitUser(accountId);
-          user.unstakeReceivedNear = user.unstakeReceivedNear.plus(unstakeAmount);
-          user.unstakedLinear = user.unstakedLinear.plus(burnedShares);
-          user.save();
+  // create price
+  let price = new Price(blockHeight.toString());
+  price.linearAmount = mintedSharesFloat;
+  price.nearAmount = stakeAmountFloat;
+  price.timestamp = timestamp.toString();
+  price.price = stakeAmountFloat.div(mintedSharesFloat);
+  price.save();
+}
 
-          // update price
-          let price = new Price(blockHeight.toString());
-          const burnedSharesFloat = BigDecimal.fromString(burnedSharesStr);
-          const unstakeSharesFloat = BigDecimal.fromString(unstakeAmountStr);
-          price.nearAmount = unstakeSharesFloat;
-          price.linearAmount = burnedSharesFloat;
-          price.timestamp = timestamp.toString();
-          price.price = unstakeSharesFloat.div(burnedSharesFloat);
-          price.save();
-        }
-      }
-    }
-  } else if (methodName == "add_liquidity" || methodName == "remove_liquidity") {
-    for (let logIndex = 0; logIndex < outcome.logs.length; logIndex++) {
-      let outcomeLog = outcome.logs[logIndex].toString();
-      if (outcomeLog.startsWith("EVENT_JSON:")) {
-        outcomeLog = outcomeLog.replace("EVENT_JSON:", "");
-        const jsonData = json.try_fromString(outcomeLog);
-        const jsonObject = jsonData.value.toObject();
-        const event = jsonObject.get("event")!;
+function handleUnstakeEvent(
+  data: TypedMap<string, JSONValue>,
+  receipt: near.ReceiptWithOutcome
+): void {
+  const timestamp = receipt.block.header.timestampNanosec;
+  const blockHeight = receipt.block.header.height;
 
-        if (
-          event.toString() == "add_liquidity" ||
-          event.toString() == "remove_liquidity"
-        ) {
-          // skip
-        } else if (event.toString() == "ft_transfer") {
-          // parse event
-          let data = jsonObject.get("data")!;
-          let dataArr = data.toArray();
-          let dataObj = dataArr[0].toObject();
-          let oldOwnerId = dataObj.get("old_owner_id")!.toString();
-          let newOwnerId = dataObj.get("new_owner_id")!.toString();
-          let amount = dataObj.get("amount")!.toString();
-          let amountInt = BigInt.fromString(amount);
+  // parse event
+  const accountId = data.get("account_id")!.toString();
+  const unstakeAmountStr = data.get("unstaked_amount")!.toString();
+  const burnedSharesStr = data.get("burnt_stake_shares")!.toString();
+  const unstakeAmount = BigInt.fromString(unstakeAmountStr);
+  const burnedShares = BigInt.fromString(burnedSharesStr);
+  const burnedSharesFloat = BigDecimal.fromString(burnedSharesStr);
+  const unstakeSharesFloat = BigDecimal.fromString(unstakeAmountStr);
 
-          // update FT Transfer event
-          let transferedEvent = FtTransfer.load(receiptHash);
-          if (transferedEvent) {
-            log.error("internal error: {}", ["transfer event"]);
-          } else {
-            transferedEvent = new FtTransfer(receiptHash);
-            transferedEvent.to = newOwnerId;
-            transferedEvent.from = oldOwnerId;
-            transferedEvent.amount = amountInt;
-            transferedEvent.timestamp = timestamp.toString();
-            transferedEvent.save();
-          }
+  // update user
+  let user = getOrInitUser(accountId);
+  user.unstakeReceivedNear = user.unstakeReceivedNear.plus(unstakeAmount);
+  user.unstakedLinear = user.unstakedLinear.plus(burnedShares);
+  user.save();
 
-          // update from user
-          let fromUser = getOrInitUser(oldOwnerId);
-          let temp = fromUser.transferedOut!;
-          temp.push(receiptHash);
-          fromUser.transferedOut = temp;
-          fromUser.save();
+  // create price
+  let price = new Price(blockHeight.toString());
+  price.nearAmount = unstakeSharesFloat;
+  price.linearAmount = burnedSharesFloat;
+  price.timestamp = timestamp.toString();
+  price.price = unstakeSharesFloat.div(burnedSharesFloat);
+  price.save();
+}
 
-          // update to user
-          let toUser = getOrInitUser(newOwnerId);
-          temp = toUser.transferedIn!;
-          temp.push(receiptHash);
-          toUser.transferedIn = temp;
-          toUser.save();
-        }
-      }
-    }
-  } else if (methodName == "ft_transfer" || methodName == "ft_transfer_call") {
-    for (let logIndex = 0; logIndex < outcome.logs.length; logIndex++) {
-      let outcomeLog = outcome.logs[logIndex].toString();
-      if (outcomeLog.startsWith("EVENT_JSON:")) {
-        outcomeLog = outcomeLog.replace("EVENT_JSON:", "");
-        let jsonData = json.try_fromString(outcomeLog);
-        let jsonObject = jsonData.value.toObject();
-        let event = jsonObject.get("event")!;
-        log.info("get ft transfer event {}", [event.toString()]);
+function handleInstantUnstakeEvent(data: TypedMap<string, JSONValue>): void {
+  // parse event
+  let accountId = data.get("account_id")!.toString();
+  let unstakeAmountStr = data.get("unstaked_amount")!.toString();
+  let unstakeLinearAmountStr = data.get("swapped_stake_shares")!.toString();
+  let unstakeAmount = BigInt.fromString(unstakeAmountStr);
+  let unstakeLinearAmount = BigInt.fromString(unstakeLinearAmountStr);
+  let feesPaid = BigInt.fromString(data.get("fee_amount")!.toString());
 
-        if (event.toString() == "ft_transfer") {
-          // parse event
-          let data = jsonObject.get("data")!;
-          let dataArr = data.toArray();
-          let dataObj = dataArr[0].toObject();
-          let oldOwnerId = dataObj.get("old_owner_id")!.toString();
-          let newOwnerId = dataObj.get("new_owner_id")!.toString();
-          let amount = dataObj.get("amount")!.toString();
-          let amountInt = BigInt.fromString(amount);
+  // update user
+  let user = getOrInitUser(accountId);
+  user.unstakeReceivedNear = user.unstakeReceivedNear.plus(unstakeAmount);
+  user.unstakedLinear = user.unstakedLinear.plus(unstakeLinearAmount);
+  user.feesPaid = user.feesPaid.plus(feesPaid);
+  user.save();
+}
 
-          // update FT transfer event
-          let transferedEvent = FtTransfer.load(receiptHash);
-          if (transferedEvent) {
-            log.error("internal error: {}", ["transfer event"]);
-          } else {
-            transferedEvent = new FtTransfer(receiptHash);
-            transferedEvent.to = newOwnerId;
-            transferedEvent.from = oldOwnerId;
-            transferedEvent.amount = amountInt;
-            transferedEvent.timestamp = timestamp.toString();
-            transferedEvent.save();
-          }
+function handleFtTransferEvent(
+  data: TypedMap<string, JSONValue>,
+  receipt: near.ReceiptWithOutcome
+): void {
+  const timestamp = receipt.block.header.timestampNanosec;
+  const receiptHash = receipt.receipt.id.toBase58();
 
-          // update from user
-          let fromUser = getOrInitUser(oldOwnerId);
-          let temp = fromUser.transferedOut!;
-          temp.push(receiptHash);
-          fromUser.transferedOut = temp;
-          fromUser.save();
+  const oldOwnerId = data.get("old_owner_id")!.toString();
+  const newOwnerId = data.get("new_owner_id")!.toString();
+  const amount = BigInt.fromString(data.get("amount")!.toString());
 
-          // update to user
-          let toUser = getOrInitUser(newOwnerId);
-          temp = toUser.transferedIn!;
-          temp.push(receiptHash);
-          toUser.transferedIn = temp;
-          toUser.save();
-        }
-      }
-    }
-  } else if (methodName == "instant_unstake") {
-    for (let logIndex = 0; logIndex < outcome.logs.length; logIndex++) {
-      let outcomeLog = outcome.logs[logIndex].toString();
-      if (outcomeLog.startsWith("EVENT_JSON:")) {
-        outcomeLog = outcomeLog.replace("EVENT_JSON:", "");
-        let jsonData = json.try_fromString(outcomeLog);
-        let jsonObject = jsonData.value.toObject();
-        let event = jsonObject.get("event")!;
+  // update event
+  let transferedEvent = FtTransfer.load(receiptHash);
+  if (!transferedEvent) {
+    transferedEvent = new FtTransfer(receiptHash);
+    transferedEvent.to = newOwnerId;
+    transferedEvent.from = oldOwnerId;
+    transferedEvent.amount = amount;
+    transferedEvent.timestamp = timestamp.toString();
+    transferedEvent.save();
 
-        if (event.toString() == "instant_unstake") {
-          // parse event
-          let data = jsonObject.get("data")!;
-          let dataArr = data.toArray();
-          let dataObj = dataArr[0].toObject();
-          let accountId = dataObj.get("account_id")!.toString();
-          let unstakeAmountStr = dataObj.get("unstaked_amount")!.toString();
-          let unstakeLinearAmountStr = dataObj
-            .get("swapped_stake_shares")!
-            .toString();
-          let unstakeAmount = BigInt.fromString(unstakeAmountStr);
-          let unstakeLinearAmount = BigInt.fromString(unstakeLinearAmountStr);
-          let feesPaidStr = dataObj.get("fee_amount")!.toString();
-          let feesPaid = BigInt.fromString(feesPaidStr);
+    // update from user
+    let fromUser = getOrInitUser(oldOwnerId);
+    let temp = fromUser.transferedOut!;
+    temp.push(receiptHash);
+    fromUser.transferedOut = temp;
+    fromUser.save();
 
-          // update user
-          let user = getOrInitUser(accountId);
-          user.unstakeReceivedNear = user.unstakeReceivedNear.plus(unstakeAmount);
-          user.unstakedLinear = user.unstakedLinear.plus(unstakeLinearAmount);
-          user.feesPaid = user.feesPaid.plus(feesPaid);
-          user.save();
-        } else if (event.toString() == "liquidity_pool_swap_fee") {
-          // parse event
-          let data = jsonObject.get("data")!;
-          let dataArr = data.toArray();
-          let dataObj = dataArr[0].toObject();
-          let poolFeesObj = dataObj.get("pool_fee_stake_shares")!;
+    // update to user
+    let toUser = getOrInitUser(newOwnerId);
+    temp = toUser.transferedIn!;
+    temp.push(receiptHash);
+    toUser.transferedIn = temp;
+    toUser.save();
+  } else {
+    log.error("internal error: {}", ["transfer event"]);
+  }
+}
 
-          // update total swap fee
-          let version = Version.load("latest".toString());
-          if (version) {
-            // increment version
-            const lastVersion = version.version;
-            const lastTotalFee = TotalSwapFee.load(lastVersion.toString())!;
-            version.version += 1;
-            version.save();
-            const lastFees = lastTotalFee.feesPaid;
+function handleLiquidityPoolSwapFeeEvent(
+  data: TypedMap<string, JSONValue>,
+  receipt: near.ReceiptWithOutcome
+): void {
+  const timestamp = receipt.block.header.timestampNanosec;
 
-            // save latest total fees
-            const newVersion = lastVersion + 1;
-            let newTotalFee = new TotalSwapFee(newVersion.toString());
-            newTotalFee.timestamp = timestamp.toString();
-            newTotalFee.feesPaid =
-              lastFees.plus(BigInt.fromString(poolFeesObj.toString()));
-            newTotalFee.save();
-          } else {
-            // save version
-            version = new Version("latest".toString());
-            version.version = 0;
-            version.save();
+  const poolFee = data.get("pool_fee_stake_shares")!;
 
-            // save initial total fees
-            let newTotalFee = new TotalSwapFee("0".toString());
-            newTotalFee.timestamp = timestamp.toString();
-            newTotalFee.feesPaid = BigInt.fromString(poolFeesObj.toString());
-            newTotalFee.save();
-          }
-        }
-      }
-    }
+  // update total swap fee
+  let version = Version.load("latest".toString());
+  if (version) {
+    // increment version
+    const lastVersion = version.version;
+    const lastTotalFee = TotalSwapFee.load(lastVersion.toString())!;
+    version.version += 1;
+    version.save();
+
+    // create total fees
+    const lastFees = lastTotalFee.feesPaid;
+    const newVersion = lastVersion + 1;
+    let newTotalFee = new TotalSwapFee(newVersion.toString());
+    newTotalFee.timestamp = timestamp.toString();
+    newTotalFee.feesPaid = lastFees.plus(BigInt.fromString(poolFee.toString()));
+    newTotalFee.save();
+  } else {
+    // creaate version
+    version = new Version("latest".toString());
+    version.version = 0;
+    version.save();
+
+    // create total fees
+    let newTotalFee = new TotalSwapFee("0".toString());
+    newTotalFee.timestamp = timestamp.toString();
+    newTotalFee.feesPaid = BigInt.fromString(poolFee.toString());
+    newTotalFee.save();
   }
 }
