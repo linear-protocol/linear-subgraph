8cb3fbcc673ea1024ccfe9d21394b5ee2ed59f45
Linguists <95207870+linguists@users.noreply.github.com>
2022-05-18 17:38:53 +0800

    style: format code

--- src/mapping/index.ts
+++ src/mapping/index.ts
@@ -8,14 +8,59 @@ import {
   handleRebalanceLiquidity,
 } from './liquidity-pool';
 
-export function handleReceipt(receipt: near.ReceiptWithOutcome): void {
-  const actions = receipt.receipt.actions;
-  for (let i = 0; i < actions.length; i++) {
-    handleAction(actions[i], receipt);
+function handleEvent(
+  method: string,
+  event: string,
+  data: TypedMap<string, JSONValue>,
+  receipt: near.ReceiptWithOutcome
+): void {
+  if (
+    method == 'stake' ||
+    method == 'deposit_and_stake' ||
+    method == 'stake_all'
+  ) {
+    if (event == 'stake') {
+      handleStake(data, receipt, method);
+    } else if (event == 'rebalance_liquidity') {
+      handleRebalanceLiquidity(data, receipt, method);
+    }
+  } else if (
+    (method == 'unstake' || method == 'unstake_all') &&
+    event == 'unstake'
+  ) {
+    handleUnstake(data, receipt, method);
+  } else if (method == 'instant_unstake' && event == 'instant_unstake') {
+    handleInstantUnstake(data);
+  } else if (
+    (method == 'ft_transfer' ||
+      method == 'ft_transfer_call' ||
+      method == 'remove_liquidity') &&
+    event == 'ft_transfer'
+  ) {
+    handleFtTransfer(data, receipt);
+  } else if (
+    method == 'instant_unstake' &&
+    event == 'liquidity_pool_swap_fee'
+  ) {
+    handleLiquidityPoolSwapFee(data, receipt);
+  } else if (method == 'storage_unregister' && event == 'ft_burn') {
+    handleFtBurn(data, receipt);
+  } else if (
+    method == 'epoch_update_rewards' ||
+    method == 'validator_get_balance_callback'
+  ) {
+    if (event == 'epoch_update_rewards') {
+      handleEpochUpdateRewards(data, receipt);
+    } else if (event == 'ft_mint') {
+      handleFtMint(data, receipt);
+    }
   }
 }
 
-function handleAction(action: near.ActionValue, receipt: near.ReceiptWithOutcome): void {
+function handleAction(
+  action: near.ActionValue,
+  receipt: near.ReceiptWithOutcome
+): void {
   if (action.kind != near.ActionKind.FUNCTION_CALL) {
     return;
   }
@@ -37,36 +82,9 @@ function handleAction(action: near.ActionValue, receipt: near.ReceiptWithOutcome
   }
 }
 
-function handleEvent(
-  method: string,
-  event: string,
-  data: TypedMap<string, JSONValue>,
-  receipt: near.ReceiptWithOutcome
-): void {
-  if (method == 'stake' || method == 'deposit_and_stake' || method == 'stake_all') {
-    if (event == 'stake') {
-      handleStake(data, receipt, method);
-    } else if (event == 'rebalance_liquidity') {
-      handleRebalanceLiquidity(data, receipt, method);
-    }
-  } else if ((method == 'unstake' || method == 'unstake_all') && event == 'unstake') {
-    handleUnstake(data, receipt, method);
-  } else if (method == 'instant_unstake' && event == 'instant_unstake') {
-    handleInstantUnstake(data);
-  } else if (
-    (method == 'ft_transfer' || method == 'ft_transfer_call' || method == 'remove_liquidity') &&
-    event == 'ft_transfer'
-  ) {
-    handleFtTransfer(data, receipt);
-  } else if (method == 'instant_unstake' && event == 'liquidity_pool_swap_fee') {
-    handleLiquidityPoolSwapFee(data, receipt);
-  } else if (method == 'storage_unregister' && event == 'ft_burn') {
-    handleFtBurn(data, receipt);
-  } else if (method == 'epoch_update_rewards' || method == 'validator_get_balance_callback') {
-    if (event == 'epoch_update_rewards') {
-      handleEpochUpdateRewards(data, receipt);
-    } else if (event == 'ft_mint') {
-      handleFtMint(data, receipt);
-    }
+export function handleReceipt(receipt: near.ReceiptWithOutcome): void {
+  const actions = receipt.receipt.actions;
+  for (let i = 0; i < actions.length; i++) {
+    handleAction(actions[i], receipt);
   }
 }
