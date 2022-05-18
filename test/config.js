846620661ad9f03774423fb41ee5abe25895233a
Linguists <95207870+linguists@users.noreply.github.com>
2022-05-18 21:57:26 +0800

    fix: update rpc and subgraph

--- test/config.js
+++ test/config.js
@@ -12,13 +12,13 @@ const config = {
     near: {
       networkId: "mainnet",
       keyStore, // optional if not signing transactions
-      nodeUrl: process.env.NEAR_CLI_MAINNET_RPC_SERVER_URL || "https://public-rpc.blockpi.io/http/near",
+      nodeUrl: process.env.NEAR_CLI_MAINNET_RPC_SERVER_URL || "https://rpc.mainnet.near.org",
       walletUrl: "https://wallet.near.org",
       helperUrl: "https://helper.near.org",
       explorerUrl: "https://explorer.near.org",
     },
     subgraph: {
-      apiUrl: "https://api.thegraph.com/subgraphs/name/ha4a4ck/linearmainnet"
+      apiUrl: "https://api.thegraph.com/subgraphs/name/linear-protocol/linear"
     },
     contract_id: "linear-protocol.near"
   },
@@ -26,7 +26,7 @@ const config = {
     near: {
       networkId: "testnet",
       keyStore, // optional if not signing transactions
-      nodeUrl: process.env.NEAR_CLI_TESTNET_RPC_SERVER_URL || "https://public-rpc.blockpi.io/http/near-testnet",
+      nodeUrl: process.env.NEAR_CLI_TESTNET_RPC_SERVER_URL || "https://rpc.testnet.near.org",
       walletUrl: "https://wallet.testnet.near.org",
       helperUrl: "https://helper.testnet.near.org",
       explorerUrl: "https://explorer.testnet.near.org",
