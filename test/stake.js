a4d61cdda13f7cee6fbe9e815f1a9e6168ad7fc8
Linguists <95207870+linguists@users.noreply.github.com>
2022-05-19 10:23:43 +0800

    style: clean up logging

--- test/stake.js
+++ test/stake.js
@@ -1,6 +1,6 @@
-const { BigNumber } = require('bignumber.js')
-const { client, loadContract } = require("./helper");
-const { queryLatestPriceFromSubgraph } = require("./price");
+const { BigNumber } = require('bignumber.js');
+const { client, loadContract } = require('./helper');
+const { queryLatestPriceFromSubgraph } = require('./price');
 
 async function queryStakeTime(accountid) {
   const getStakeTimeQuery = `
@@ -9,18 +9,18 @@ async function queryStakeTime(accountid) {
       id
       firstStakingTime
     }
-  }`
+  }`;
   // console.log(getStakeTimeQuery)
-  let data = await client.query(getStakeTimeQuery).toPromise()
-  let queryData = data.data
+  let data = await client.query(getStakeTimeQuery).toPromise();
+  let queryData = data.data;
   if (queryData == null) {
-    throw new Error("fail to query price");
+    throw new Error('fail to query price');
   }
-  const timestampInt = Number(queryData.users[0].firstStakingTime.toString())
-  const unixTimestamp = timestampInt / 1000000
-  const date = new Date(unixTimestamp)
-  console.log("user first stake time: ", date)
-  return queryData.users[0]
+  const timestampInt = Number(queryData.users[0].firstStakingTime.toString());
+  const unixTimestamp = timestampInt / 1000000;
+  const date = new Date(unixTimestamp);
+  console.log('user first stake time: ', date);
+  return queryData.users[0];
 }
 
 async function getTransferIncome(accountID) {
@@ -33,26 +33,27 @@ async function getTransferIncome(accountID) {
         transferedOutShares
         transferedOutValue
       }
-  }`
+  }`;
   // console.log(getTransferEvent)
-  let data = await client.query(getTransferEvent).toPromise()
-  let queryData = data.data
+  let data = await client.query(getTransferEvent).toPromise();
+  let queryData = data.data;
   //console.log(queryData.users[0])
   if (queryData == null) {
-    throw new Error("Fail to query transfer event")
+    throw new Error('Fail to query transfer event');
   }
-  const latestPrice = await queryLatestPriceFromSubgraph()
+  const latestPrice = await queryLatestPriceFromSubgraph();
   //console.log(latestPrice.price)
-  const transferInShares = queryData.users[0].transferedInShares
-  const tranfserInValue = queryData.users[0].transferedInValue
-  const transferOutShares = queryData.users[0].transferedOutShares
-  const tranfserOutValue = queryData.users[0].transferedOutValue
+  const transferInShares = queryData.users[0].transferedInShares;
+  const tranfserInValue = queryData.users[0].transferedInValue;
+  const transferOutShares = queryData.users[0].transferedOutShares;
+  const tranfserOutValue = queryData.users[0].transferedOutValue;
   //console.log(transferInShares,tranfserInValue)
-  let transferInReward =  latestPrice.price * transferInShares - tranfserInValue;
-  let transferOutReward = latestPrice.price * transferOutShares - tranfserOutValue;
-  
-  console.log("transfer reward: ",transferInReward - transferOutReward)
-  return transferInReward - transferOutReward
+  let transferInReward = latestPrice.price * transferInShares - tranfserInValue;
+  let transferOutReward =
+    latestPrice.price * transferOutShares - tranfserOutValue;
+
+  // console.log("transfer reward: ",transferInReward - transferOutReward)
+  return transferInReward - transferOutReward;
 }
 
 async function getUserIncome(accountId, flag) {
@@ -66,25 +67,30 @@ async function getUserIncome(accountId, flag) {
         unstakeReceivedNear
         feesPaid
       }
-    }`
+    }`;
   // console.log(getIncomeQuery)
-  let data = await client.query(getIncomeQuery).toPromise()
+  let data = await client.query(getIncomeQuery).toPromise();
   //console.log(data)
-  let queryData = data.data.users[0]
+  let queryData = data.data.users[0];
   if (queryData == null) {
-    throw new Error("fail to query user")
+    throw new Error('fail to query user');
   }
-  const latestPrice = await queryLatestPriceFromSubgraph()
-  const price1 = new BigNumber(latestPrice.price)
-  const mintedLinear = new BigNumber(queryData.mintedLinear)
-  const stakedNear = new BigNumber(queryData.stakedNear)
-  const unstakedLinear = new BigNumber(queryData.unstakedLinear)
-  const unstakedGetNEAR = new BigNumber(queryData.unstakeReceivedNear)
-  const fessPaid = new BigNumber(queryData.feesPaid)
+  const latestPrice = await queryLatestPriceFromSubgraph();
+  const price1 = new BigNumber(latestPrice.price);
+  const mintedLinear = new BigNumber(queryData.mintedLinear);
+  const stakedNear = new BigNumber(queryData.stakedNear);
+  const unstakedLinear = new BigNumber(queryData.unstakedLinear);
+  const unstakedGetNEAR = new BigNumber(queryData.unstakeReceivedNear);
+  const fessPaid = new BigNumber(queryData.feesPaid);
   const currentLinear = mintedLinear.minus(unstakedLinear);
   const transferReward = await getTransferIncome(accountId);
   const tfReward = new BigNumber(transferReward);
-  const reward = currentLinear.times(price1).integerValue().minus(stakedNear).plus(unstakedGetNEAR).plus(tfReward);
+  const reward = currentLinear
+    .times(price1)
+    .integerValue()
+    .minus(stakedNear)
+    .plus(unstakedGetNEAR)
+    .plus(tfReward);
   // console.log("calc [subgraph]",
   //   mintedLinear.toString(),
   //   unstakedLinear.toString(),
@@ -94,33 +100,36 @@ async function getUserIncome(accountId, flag) {
   // );
 
   if (flag) {
-    const rewardFinal = reward.plus(fessPaid)
-    console.log("rewards [subgraph with fee] =\t\t %s NEAR", rewardFinal.div(10 ** 24).toFixed(8))
-    return rewardFinal
+    const rewardFinal = reward.plus(fessPaid);
+    console.log(
+      'rewards [subgraph with fee] =\t\t %s NEAR',
+      rewardFinal.div(10 ** 24).toFixed(8)
+    );
+    return rewardFinal;
   } else {
-    console.log("rewards [subgraph without fee] =\t %s NEAR", reward.div(10 ** 24).toFixed(8))
-    return reward
+    console.log(
+      'rewards [subgraph without fee] =\t %s NEAR',
+      reward.div(10 ** 24).toFixed(8)
+    );
+    return reward;
   }
 }
 
 async function getDeposits(accountId) {
-  const result = await fetch(`https://api.linearprotocol.org/deposits/${accountId}`);
+  const result = await fetch(
+    `https://api.linearprotocol.org/deposits/${accountId}`
+  );
   const json = await result.json();
   return json.deposits;
-};
+}
 
 async function getStakingReward(accountId) {
   const contract = await loadContract();
-  const [
-    liNearPrice,
-    account,
-    linearBalance,
-    deposits
-  ] = await Promise.all([
+  const [liNearPrice, account, linearBalance, deposits] = await Promise.all([
     contract.ft_price(),
     contract.get_account({ account_id: accountId }),
     contract.ft_balance_of({ account_id: accountId }),
-    getDeposits(accountId)
+    getDeposits(accountId),
   ]);
 
   const near_reward = BigNumber(linearBalance)
@@ -138,7 +147,10 @@ async function getStakingReward(accountId) {
   //   deposits.near.toString(),
   // );
 
-  console.log("rewards [indexer] =\t\t\t %s NEAR", near_reward.div(10 ** 24).toFixed(8));
+  console.log(
+    'rewards [indexer] =\t\t\t %s NEAR',
+    near_reward.div(10 ** 24).toFixed(8)
+  );
   return near_reward;
 }
 
@@ -146,22 +158,28 @@ async function stakingRewardsDiff(accountId) {
   const [
     rewards_subgraph_with_fee,
     // rewards_subgraph,
-    rewards_indexer
+    rewards_indexer,
   ] = await Promise.all([
     getUserIncome(accountId, true),
     // getUserIncome(accountId, false),
-    getStakingReward(accountId)
-  ])
+    getStakingReward(accountId),
+  ]);
   // console.log("diff [subgraph - indexer] =\t\t %s NEAR", rewards_subgraph.minus(rewards_indexer).div(10 ** 24).toFixed(8));
-  console.log("diff [subgraph with fee - indexer] =\t %s NEAR", rewards_subgraph_with_fee.minus(rewards_indexer).div(10 ** 24).toFixed(8));
+  console.log(
+    'diff [subgraph with fee - indexer] =\t %s NEAR',
+    rewards_subgraph_with_fee
+      .minus(rewards_indexer)
+      .div(10 ** 24)
+      .toFixed(8)
+  );
 }
 
 async function test() {
   const accountIds = [
-    "cookiemonster.near",
-    "retitre.near",
-    "calmincome1.near",
-    "linguists.near",
+    'cookiemonster.near',
+    'retitre.near',
+    'calmincome1.near',
+    'linguists.near',
   ];
   for (const accountId of accountIds) {
     console.log('\nstaking rewards: account: ', accountId);
