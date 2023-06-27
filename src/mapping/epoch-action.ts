b57ee4ab36901c05fd7fc294d64cdedbf5fc723d
0xdefacto <107129842+0xdefacto@users.noreply.github.com>
2023-06-27 15:41:06 +0800

    feat: record epoch unstaked amounts of validators (#13)

--- src/mapping/epoch-action.ts
+++ src/mapping/epoch-action.ts
@@ -1,5 +1,6 @@
-import { near, JSONValue, TypedMap, BigDecimal } from '@graphprotocol/graph-ts';
+import { near, BigInt, JSONValue, TypedMap, BigDecimal } from '@graphprotocol/graph-ts';
 import { updatePrice } from '../helper/price';
+import { ValidatorEpochInfo } from '../../generated/schema';
 
 export function handleEpochUpdateRewards(
   method: string,
@@ -9,4 +10,33 @@ export function handleEpochUpdateRewards(
 ): void {
   const rewards = BigDecimal.fromString(data.get('rewards')!.toString());
   updatePrice(event, method, receipt, rewards, BigDecimal.zero());
+}
+
+export function handleEpochUnstakeSuccess(
+  data: TypedMap<string, JSONValue>,
+  receipt: near.ReceiptWithOutcome
+): void {
+  const amount = BigInt.fromString(data.get('amount')!.toString());
+
+  const epochId = receipt.block.header.epochId.toBase58();
+  const validatorId = data.get('validator_id')!.toString();
+  const id = epochId + '#' + validatorId;
+
+  const entity = ValidatorEpochInfo.load(id);
+
+  if (!entity) {
+    const entity = new ValidatorEpochInfo(id);
+
+    entity.epochId = epochId;
+    entity.validatorId = validatorId;
+    entity.epochUnstakedAmount = amount;
+
+    entity.save();
+  } else {
+    // Logic here should be redundant because one validator can only be epoch_unstake() once.
+    // But just in case, we still keep it here.
+    entity.epochUnstakedAmount = entity.epochUnstakedAmount.plus(amount);
+
+    entity.save();
+  }
 }
