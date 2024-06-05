b765cf84685fb167f5b68811ccc2c92a8935cae9
Linguists <95207870+linguists@users.noreply.github.com>
2024-06-05 09:57:53 +0800

    chore: update subgraph urls

--- test/config.js
+++ test/config.js
@@ -1,40 +1,46 @@
-const { keyStores } = require("near-api-js");
-const { BigNumber } = require('bignumber.js')
+const { keyStores } = require('near-api-js');
+const { BigNumber } = require('bignumber.js');
 
 const keyStore = new keyStores.InMemoryKeyStore();
 
 BigNumber.config({
-  DECIMAL_PLACES: 64
-})
+  DECIMAL_PLACES: 64,
+});
 
 const config = {
   mainnet: {
     near: {
-      networkId: "mainnet",
+      networkId: 'mainnet',
       keyStore, // optional if not signing transactions
-      nodeUrl: process.env.NEAR_CLI_MAINNET_RPC_SERVER_URL || "https://rpc.mainnet.near.org",
-      walletUrl: "https://wallet.near.org",
-      helperUrl: "https://helper.near.org",
-      explorerUrl: "https://explorer.near.org",
+      nodeUrl:
+        process.env.NEAR_CLI_MAINNET_RPC_SERVER_URL ||
+        'https://rpc.mainnet.near.org',
+      walletUrl: 'https://wallet.near.org',
+      helperUrl: 'https://helper.near.org',
+      explorerUrl: 'https://explorer.near.org',
     },
     subgraph: {
-      apiUrl: "https://api.thegraph.com/subgraphs/name/linear-protocol/linear"
+      apiUrl:
+        'https://api.studio.thegraph.com/query/76854/linear/version/latest',
     },
-    contract_id: "linear-protocol.near"
+    contract_id: 'linear-protocol.near',
   },
   testnet: {
     near: {
-      networkId: "testnet",
+      networkId: 'testnet',
       keyStore, // optional if not signing transactions
-      nodeUrl: process.env.NEAR_CLI_TESTNET_RPC_SERVER_URL || "https://rpc.testnet.near.org",
-      walletUrl: "https://wallet.testnet.near.org",
-      helperUrl: "https://helper.testnet.near.org",
-      explorerUrl: "https://explorer.testnet.near.org",
+      nodeUrl:
+        process.env.NEAR_CLI_TESTNET_RPC_SERVER_URL ||
+        'https://rpc.testnet.near.org',
+      walletUrl: 'https://wallet.testnet.near.org',
+      helperUrl: 'https://helper.testnet.near.org',
+      explorerUrl: 'https://explorer.testnet.near.org',
     },
     subgraph: {
-      apiUrl: "https://api.thegraph.com/subgraphs/name/linear-protocol/linear-testnet"
+      apiUrl:
+        'https://api.studio.thegraph.com/query/76854/linear-testnet/version/latest',
     },
-    contract_id: "linear-protocol.testnet"
+    contract_id: 'linear-protocol.testnet',
   },
 };
 
